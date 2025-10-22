4cf1c277819eb5e8844e6e1b6977df916046a465
Ram <39217224+Rammurthy5@users.noreply.github.com>
2025-10-22 20:42:02 +0100

    Create subset_sum_I.py

---
+++ subset_sum_I.py
@@ -0,0 +1,10 @@
+def print_all_subset_sum(ind, arr, t_arr, summ):
+  if ind ==len(arr): # stop when the index is out of bounds
+    return [summ]
+  t_arr.append(arr[ind])
+  include = print_all_subset_sum(ind+1, arr, t_arr, summ+arr[ind]) # include the current element and recurse the next ind
+  t_arr.pop()
+  exclude = print_all_subset_sum(ind+1, arr, t_arr, summ) # excluse the current ele and recurse the next ind
+  return include + exclude
+
+print(print_all_subset_sum(0, [3,1,2],[],0))
