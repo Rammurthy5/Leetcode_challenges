415542d4a65842cb859d7f19bcfb8da34ec50ef9
Ram <39217224+Rammurthy5@users.noreply.github.com>
2025-10-23 21:12:53 +0100

    Implement in-place permutation generation method
    Added a second approach for generating permutations using in-place swapping, optimizing space complexity.

--- recursion_all_permutations.py
+++ recursion_all_permutations.py
@@ -1,4 +1,5 @@
 # time complexity is O(n! * n) n! for the permutation factorial, n for the work in each permutation
+# space complexity is O(n! * n) because we are storing it on hash map. 
 from collections import defaultdict
 
 
@@ -19,4 +20,19 @@ def print_all_permutations(arr, t_arr, hasher):
     return result
 
 
+# time complexity is same as above but space complexity is optimised by using swap method in-place on original array.
+def print_all_permutations_approach2(ind, arr):
+    if ind == len(arr):
+        return [arr.copy()]
+
+    result = []
+
+    for i in range(ind, len(arr)):
+            arr[ind], arr[i] = arr[i], arr[ind]
+            result += print_all_permutations_approach2(ind+1, arr)
+            arr[ind], arr[i] = arr[i], arr[ind]
+    return result
+
+
 print(print_all_permutations([2,3,5], [], defaultdict(bool)))
+print(print_all_permutations(0, [2,3,5]))
