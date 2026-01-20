6a451031e48cead72f322f5b78e07e529dc9f0ec
Ram <39217224+Rammurthy5@users.noreply.github.com>
2026-01-20 21:30:21 +0000

    Update BuySellStock_withFee.py

--- BuySellStock_withFee.py
+++ BuySellStock_withFee.py
@@ -6,5 +6,16 @@ class Solution:
             cash = max(cash, hold+prices[day]-fee)
             hold = max(hold, cash-prices[day])
         return cash
-        
- # time O(n) space O(n)
+
+    # time O(n) space O(n)
+
+    def dp_approach_no_fee(self, prices: List[int]) -> int:
+        mini = prices[0]
+        max_profit=float('-inf')
+        for i in range(1, len(prices)):
+            if prices[i]-mini>max_profit:
+                max_profit=prices[i]-mini
+            mini = min(mini, prices[i])
+        print(max_profit)
+
+# t.c O(n) space O(1)
