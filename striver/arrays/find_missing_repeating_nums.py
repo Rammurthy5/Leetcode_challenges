a6d6db667ac2bcbc75653290dfe39560036883bd
Ram <39217224+Rammurthy5@users.noreply.github.com>
2026-02-10 14:23:06 +0000

    Update find_missing_repeating_nums.py

--- striver/arrays/find_missing_repeating_nums.py
+++ striver/arrays/find_missing_repeating_nums.py
@@ -15,32 +15,19 @@ def find_missing_repeating_nums(nums):
 find_missing_repeating_nums(nums)
 
 # time complexity O(n) and aux com O(1)
+# math based approach. xsq - ysq = (x+y)(x-y). find x and y. 
 
 def find_missing_repeating_nums(nums):
     n = len(nums)
-    xor_all = 0
-
-    for i in range(n):
-        xor_all ^= nums[i]
-        xor_all ^= (i + 1)
-
-    rightmost_set_bit = xor_all & -xor_all
-
-    x = y = 0
-    for num in nums:
-        if num & rightmost_set_bit:
-            x ^= num
-        else:
-            y ^= num
-
-    for i in range(1, n + 1):
-        if i & rightmost_set_bit:
-            x ^= i
-        else:
-            y ^= i
-
-    # determine which is repeating
-    if nums.count(x) == 2:
-        return [x, y]
-    else:
-        return [y, x]
+    local_nums = range(1, n + 1)
+    sum_of_nums = sum(nums)
+    sum_of_local_nums = sum(local_nums)
+    sum_of_squares_of_nums = sum(x ** 2 for x in nums)
+    sum_of_squares_of_local_nums = sum(x ** 2 for x in local_nums)
+    x_minus_y = sum_of_nums - sum_of_local_nums
+    xsquared_minus_ysquared = sum_of_squares_of_nums - sum_of_squares_of_local_nums
+    x_plus_y = xsquared_minus_ysquared // x_minus_y
+    x = (x_minus_y + x_plus_y) // 2
+    y = x_plus_y - x
+    print(y, x)
+find_missing_repeating_nums(nums)
